1bc40d3b43c94d12919a1e84ddfbd37ae43749c9
JIC1444 <158279190+JIC1444@users.noreply.github.com>
2024-06-10 15:15:54 +0100

    Update LaneLinesRecogniser.py

--- Lane Recogniser/LaneLinesRecogniser.py
+++ Lane Recogniser/LaneLinesRecogniser.py
@@ -7,8 +7,8 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
-import json
 import pandas as pd
+import random
 
 
 """
@@ -37,50 +37,61 @@ extra images? Or use them for testing purposes?
 """
 
 subdirs = [x[0] for x in os.walk("SELF DRIVING CAR/DATA/TUSimple/train_set/clips")]
-print(subdirs)
+#print(subdirs)
 
-def delete_image(image_dir):
+"""def delete_image(image_dir):
     for img in image_dir:
         os.remove(img)
 
 for folder in [1,1,1,1]:
-    if folder != 1:
-        delete_image(folder)
+    if folder != 20:
+        delete_image(folder)"""
 
 
 #Test that the lane lines are drawn in the correct place
-sample_img_dir = "SELF DRIVING CAR/DATA/TUSimple/train_set/clips/0313-1/60/20.jpg"
-sample_seg_label_dir = "SELF DRIVING CAR/DATA/TUSimple/train_set/seg_label/0313-1/60/20.png"
+sample_img_dir = "SELF DRIVING CAR/DATA/TUSimple/train_set/clips/0313-1/6040/20.jpg"
+sample_seg_label_dir = "SELF DRIVING CAR/DATA/TUSimple/train_set/seg_label/0313-1/6040/20.png"
 sample_label_data_dir = "SELF DRIVING CAR/DATA/TUSimple/train_set/label_data_0313.ndjson"
 
-def draw_precalced_lines(img_dir, seg_label_dir, label_data_dir, label_data_line):
+#Function takes in the ndjson and the line, then creates the correct directory for the image and seg_label 
+def match_video_to_lane_data(lane_df, line): 
+    raw_file = lane_df["raw_file"][line]
+    folder = "SELF DRIVING CAR/DATA/TUSimple/train_set/"
+    img_dir = os.path.join(folder, raw_file)
+    raw_file = raw_file[6:] #Removes the "clips" part of the raw file dir
+    raw_file = raw_file[0:len(raw_file)-3] #Removes jpg, since the seg label is a png!
+    raw_file = raw_file + "png"
+    seg_label_dir = os.path.join(folder, "seg_label/", raw_file)
+    return img_dir, seg_label_dir
+
+def draw_precalced_lines(lane_df, label_data_line: int): 
+    img_dir, seg_label_dir = match_video_to_lane_data(lane_df, label_data_line) 
     img = np.asarray(Image.open(img_dir))
-    seg_label =  np.asarray(Image.open(seg_label_dir))
-    plt.imshow(img)
-    plt.show()
-
+    seg_label = np.asarray(Image.open(seg_label_dir))
     seg_label = cv2.convertScaleAbs(seg_label, 1.5, 10) #Increases constrast and brightness
-    plt.imshow(seg_label)
-    plt.show()
 
-    lane_data = pd.read_json(label_data_dir, lines=True)
+    lane_x_coords = np.array(lane_df["lanes"][label_data_line]) #List of x-coordinates of the lanes
+    lane_y_coords = np.array(lane_df["h_samples"][label_data_line]) #List of y-coordinates of the lanes
+    #Shape of lane_x and lane_y -> (4, 48) (48,)
 
-    #with open(label_data_dir, "r") as file:
-        #lane_data = json.load(file)
-    lane_x_coords = lane_data["lanes"][label_data_line] #List of x-coordinates of the lanes
-    lane_y_coords = lane_data["h_samples"][label_data_line] #List of y-coordinates of the lanes
-    raw_file = lane_data["raw_file"][label_data_line] 
-
-    print(lane_x_coords.shape,lane_y_coords.shape)
-
-    for i in range(0, len(lane_x_coords)):
-        plt.plot(lane_x_coords[i],lane_y_coords[i])
-    plt.imshow(img)
+    fig, ax = plt.subplots()
+    ax.imshow(img)
+    colors = ["r","r","b","g"] #In the x_coords 4D array, the first two arrays are the immediate lane lines 
+    #and then 3rd is the one to the left and the 4th is the one to the right (there is max 4 dims in the x_coords)
+    for j, color in enumerate(colors): 
+        for i in range(0, len(lane_y_coords)):
+            if lane_x_coords[j][i] == -2: #x = -2 indicates a non value.
+                continue
+            else:
+                print(lane_x_coords[j][i], lane_y_coords[i])
+                ax.scatter(x = lane_x_coords[j][i], y = lane_y_coords[i], 
+                            c = color, s = 20)
     plt.show()
     
-
-    
-draw_precalced_lines(sample_img_dir,sample_seg_label_dir, sample_label_data_dir, 0)
+#Example
+r = random.randint(0,2858)
+lane_data = pd.read_json(sample_label_data_dir, lines=True)
+draw_precalced_lines(lane_data, r) #Careful, since 2858 is the length of 0313 label data
 
 
 class LaneLinesDataset(Dataset):
@@ -98,25 +109,37 @@ class LaneLinesDataset(Dataset):
         
         pass
 
-#Processing image requires: resize, grayscale, GaussianBlur, hough transform
-def grayscale(img):
+#Processing image requires: resize, grayscale, GaussianBlur, region of interest
+#Leaving out hough transform at first as I want to see whether the CNN can make curved lines without me having
+#to specifically code it in using the hough transform or a variant of it
+    
+def grayscale(img): #Reduced complexity in greyscale
     img = img.resize() #Find what size we need
     return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-
-def gaussian_blur(img):
+def gaussian_blur(img): #Takes an input using the Gaussian distribuiton of the pixel colour around the target pixel
     return cv2.GaussianBlur(img)
 
-def hough_transform(img):
-    pass
+def region_of_interest(img): #Take a triangular or trapezium-shaped mask of the image
+    height, width = img.shape 
+    triangle = np.array([[(0, height), (650, 150), (width, height)]])
+    mask = np.zeros_like(img) #Creates black image with the same dimensions as original image
+    mask = cv2.fillPoly(mask, triangle, 255) #255 is white (hence why the numpy array is 0 everywhere else)
+    mask = cv2.bitwise_and(img, mask) #Bitwise_and finds the parts of the image which are similar and different and groups them
+    return mask
 
-def process_image(img):
+def process_image(img_dir):
+    img = np.asarray(Image.open(img_dir))
     gray = grayscale(img)
     gauss = gaussian_blur(gray)
+    processed_img = region_of_interest(gauss)
+    plt.imshow(processed_img)
+    return processed_img
+
+process_image()
 
 
-
-class CNN():
+class CNN(nn.Module):
     def __init__(self):
         super.__init__()
         pass
